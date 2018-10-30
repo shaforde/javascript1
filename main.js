48677d87330ac0bc451a8e18d36977ead3023d02
stadmin <stadmin@nyc.yearup.org>
2018-10-30 15:32:19 -0400

    *

---
+++ main.js
@@ -0,0 +1,16 @@
+var prices=[33, 26, 99, 120, 12, 45];
+var sum = 0;
+// Your code here
+for(var=0; i<
+  for (i = 0; i < 5; i++) {
+
+
+
+
+let crows=99;
+while(crows>0){
+  if(crows == 1){
+    console.log("1 crow on the wall. 1 crow");
+    console.log 
+  }
+}
